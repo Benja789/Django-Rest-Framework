4879d80fc2fffd27d1f16c86c36e34beecc8d221
Benja789 <molina.benjamin70@gmail.com>
2021-09-14 16:57:58 -0600

    Configuracion de metodos post y get

--- Rest_Framework/apps/users/api/api.py
+++ Rest_Framework/apps/users/api/api.py
@@ -1,14 +1,33 @@
+import re
+from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from apps.users.api.serializer import UserSerializer
 from apps.users.models import User
-
+#Con decorador
+from rest_framework.decorators import api_view
  
 #Creacion de la clase
-class UserAPIView (APIView):
+"""class UserAPIView (APIView):
     #Metodo get y primera vista de rest framework
     def get(self,request):
         users = User.objects.all()                              #Consulta
         user_serializer = UserSerializer(users, many=True)      #Como se serializa
-        return  Response (user_serializer.data)                 #la informacion se encuentra en data
+        return  Response (user_serializer.data)                 #la informacion se encuentra en data"""
+
+#Decorador
+@api_view(['GET', 'POST'])
+def user_api_view(request):
+    if request.method == 'GET':
+        users = User.objects.all()                              #Consulta
+        user_serializer = UserSerializer(users, many=True)      #Como se serializa
+        return  Response (user_serializer.data)                 #la informacion se encuentra en data"
+    elif request.method =='POST':
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        else:
+            return Response(user_serializer.errors)
+#        print(request.data)
 
--- Rest_Framework/apps/users/api/urls.py
+++ Rest_Framework/apps/users/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView
+#from apps.users.api.api import UserAPIView Clase
+from apps.users.api.api  import user_api_view
 
 urlpatterns =[
-        path('usuario/', UserAPIView.as_view(), name='usuario_api')
+#        path('usuario/', UserAPIView.as_view(), name='usuario_api') Clase
+        path('usuario/', user_api_view, name='usuario_api'), #Funcion
 ]
